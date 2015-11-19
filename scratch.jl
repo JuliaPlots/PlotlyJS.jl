d85b59a1d2335d71de068c8bd2267ac7bca0e0c0
Spencer Lyon <spencerlyon2@gmail.com>
2015-11-19 01:03:32 -0500

    fixed scratch

--- scratch.jl
+++ scratch.jl
@@ -1,24 +1,16 @@
 using JSON
 
-js_dict = JSON.parsefile(joinpath(dirname(@__FILE__, "deps", "plotschema.json")))
+js_dict = JSON.parsefile(joinpath(dirname(@__FILE__), "deps", "plotschema.json"))
 
 # recursively determine all unique values for a key in the Dict form of the json
-get_unique(::Any, key, the_set) = nothing
+get_unique!(::Any, key, the_set) = nothing
 
 function get_unique!(d::Dict, key, the_set=Set())
     haskey(d, key) &&  push!(the_set, d[key])
-    map(x->get_unique(x, key, the_set), values(d))
-    return the_set
+    map(x->get_unique!(x, key, the_set), values(d))
+    the_set
 end
 
 valtypes = get_unique!(js_dict, "valType")
 defaults = get_unique!(js_dict, "dflt")
-
-immutable Angle
-    x::Float16
-
-    function Angle(x::Float64)
-        (x < -180 || x > 180) && error("Angle must be between -180 and 180")
-        new(x)
-    end
-end
+roles = get_unique!(js_dict, "role")
