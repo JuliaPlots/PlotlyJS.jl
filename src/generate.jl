e30a364011ac13f2865b9890ef413b22cc44d854
Spencer Lyon <spencerlyon2@gmail.com>
2015-11-20 15:19:25 -0500

    Done parsing traces

--- src/generate.jl
+++ src/generate.jl
@@ -16,18 +16,20 @@ end
 function verify_schema_knowledge()
     valtypes = get_unique!(schema, "valType")
     roles = get_unique!(schema, "role")
-    other_opts = unique(vcat(get_unique!(schema["defs"]["valObjects"], "otherOpts")...))
+    opts = unique(vcat(get_unique!(schema["defs"]["valObjects"], "otherOpts")...))
+    opts = unique(vcat(opts,
+                       get_unique!(schema["defs"]["valObjects"], "requiredOpts")...))
 
     # make sure we don't have any roles or otherOpts we don't know about
     @assert isempty(symdiff(roles, Set(["data", "style", "info", "object"])))
-    @assert isempty(symdiff(other_opts, ["dflt", "min", "max", "arrayOk", "noBlank",
-                                         "strict", "values", "extras",
-                                         "coerceNumber"]))
+    @assert isempty(symdiff(opts, ["dflt", "min", "max", "arrayOk", "noBlank",
+                                   "strict", "values", "extras", "flags", "items",
+                                    "coerceNumber"]))
 
     # make sure that valtypes is the same as the keys in our defs.valObjects:
     @assert isempty(symdiff(valtypes, collect(keys(schema["defs"]["valObjects"]))))
 
-    valtypes, roles, other_opts
+    valtypes, roles, opts
 end
 
 verify_schema_knowledge()
@@ -45,7 +47,7 @@ end
 abstract AbstractOpt
 
 for nm in [:_Dflt, :_Min, :_Max, :_ArrayOk, :_NoBlank, :_Strict, :_Values,
-           :_Extras, :_CoerceNumber]
+           :_Extras, :_CoerceNumber, :_Flags, :_Items]
     @eval immutable $(nm) <: AbstractOpt; value; end
 end
 
@@ -96,10 +98,14 @@ end
 type TraceDescription{TAD<:AbstractAttribueDescription}
     name::Symbol
     attributes::Vector{TAD}
+    layout_attributes::Vector{TAD}
     docstring::AbstractString
 end
 
 function _parse_object(name::Symbol, d::Dict)
+    # remove anything deprcated. We just won't implement it
+    pop!(d, "_deprecated", "")
+
     # extract a docstring, if any
     docstring = pop!(d, "description", "")
 
@@ -120,6 +126,10 @@ function _parse_object(name::Symbol, d::Dict)
 end
 
 function _parse_val(name::Symbol, d::Dict, role::Symbol)
+    # remove anything deprcated. We just won't implement it
+    pop!(d, "_deprecated", "")
+
+
     # parse this string to one of our valtype instances
     valtype = eval(Expr(:call, symbol("_", ucfirst(pop!(d, "valType")))))
 
@@ -139,13 +149,18 @@ function _parse_val(name::Symbol, d::Dict, role::Symbol)
     ValAttributeDescription(name, role, valtype, opts, docstring)
 end
 
-
 # debugging helper. Method to dispatch on anything
 parse_attr(name, d) = error("I have name: $name\nd: $d")
 
 function parse_attr(name::Symbol, d::Dict)
+    # remove anything deprcated. We just won't implement it
+    pop!(d, "_deprecated", "")
+
     # make sure we really have an attribute here
-    haskey(d, "role") || error("Not an attribute")
+    if !haskey(d, "role")
+        @show name, d
+        error("Not an attribute")
+    end
 
     # parse role
     role = symbol(pop!(d, "role"))
@@ -154,5 +169,46 @@ function parse_attr(name::Symbol, d::Dict)
                       _parse_val(name, d, role)
 end
 
-d_val = deepcopy(schema["traces"]["scatter"]["attributes"]["visible"])
-d_obj = deepcopy(schema["traces"]["scatter"]["attributes"]["line"])
+function _parse_all_attrs(attrs::Dict)
+    attributes = Array(AbstractAttribueDescription, length(attrs))
+    for (i, (k, v)) in enumerate(attrs)
+        attributes[i] = parse_attr(symbol(k), v)
+        pop!(attrs, k)
+    end
+    # make sure we parsed all the attributes
+    @assert isempty(attrs)
+    attributes
+end
+
+function parse_trace(name::Symbol, d::Dict)
+    # remove anything deprcated. We just won't implement it
+    pop!(d, "_deprecated", "")
+
+    # remove redundant `type` (this info is in `name`)
+
+    # extract docstring
+    docstring = pop!(d, "description", "")
+
+    # parse all attributes
+    attrs = pop!(d, "attributes")
+    pop!(attrs, "type")
+    pop!(attrs, "_deprecated", "")
+    attributes = _parse_all_attrs(attrs)
+
+    # parse all layoutAttributes
+    attrs = pop!(d, "layoutAttributes", Dict())
+    pop!(attrs, "_deprecated", "")
+    pop!(attrs, "type", "")
+    layout_attributes = _parse_all_attrs(attrs)
+
+    # scatter3d has this strange "hrName" field. Throw it out
+    pop!(d, "hrName", "")
+
+    # make sure we parsed everything in this trace
+    @assert isempty(d)
+
+    TraceDescription(name, attributes, layout_attributes, docstring)
+end
+
+parse_traces(schema_traces::Dict) =
+    TraceDescription[parse_trace(symbol(k), v) for (k, v) in schema_traces]
