9eea3174f80012038276340d148be9fd2ad8951d
Spencer Lyon <spencerlyon2@gmail.com>
2015-12-02 09:31:55 -0500

    added  functions to fill in  field

--- src/api.jl
+++ src/api.jl
@@ -5,3 +5,9 @@ function Base.resize!(p::Plot)
     sz = size(p)
     size(p, sz[1]+10, sz[2]+25)
 end
+
+for t in [:histogram, :scatter3d, :surface, :mesh3d, :bar, :histogram2d,
+          :histogram2dcontour, :scatter, :pie, :heatmap, :contour,
+          :scattergl, :box, :area, :scattergeo, :choropleth]
+    @eval $t(;kwargs...) = GenericTrace(string($t); kwargs...)
+end
