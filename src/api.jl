2740673d71d016e426060b94b17efe2edf6078ee
Spencer Lyon <spencerlyon2@gmail.com>
2015-12-02 20:18:11 -0500

    export api helper methods. Also ignore vscode

--- src/api.jl
+++ src/api.jl
@@ -10,4 +10,5 @@ for t in [:histogram, :scatter3d, :surface, :mesh3d, :bar, :histogram2d,
           :histogram2dcontour, :scatter, :pie, :heatmap, :contour,
           :scattergl, :box, :area, :scattergeo, :choropleth]
     @eval $t(;kwargs...) = GenericTrace(string($t); kwargs...)
+    eval(Expr(:export, t))
 end
