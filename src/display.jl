c2c288198df6635d35bc604949abea6efa43ad3d
Spencer Lyon <spencerlyon2@gmail.com>
2015-12-07 09:00:54 -0500

    enabled notebook output

--- src/display.jl
+++ src/display.jl
@@ -55,6 +55,25 @@ function Base.show(p::Plot)
     p
 end
 
+# ---------------------- #
+# Jupyter notebook setup #
+# ---------------------- #
+
+# if we're in IJulia call setupnotebook to load js and css
+if isdefined(Main, :IJulia) && Main.IJulia.inited
+    # the first script is some hack I needed to do in order for the notebook
+    # to not complain about Plotly being undefined
+    display("text/html", """
+        <script type="text/javascript">
+            require=requirejs=define=undefined;
+        </script>
+        <script type="text/javascript">
+            $(open(readall, _js_path, "r"))
+        </script>
+     """)
+    display("text/html", "<p>Plotly javascript loaded.</p>")
+end
+
 # -------------- #
 # Javascript API #
 # -------------- #
