472a061cad586d49b665ab8f7a3cf9de0362d4ba
Spencer Lyon <spencerlyon2@gmail.com>
2015-12-17 10:28:30 -0700

    updated savefig mechanism

--- src/api.jl
+++ src/api.jl
@@ -21,17 +21,16 @@ Base.copy(gt::GenericTrace) = GenericTrace(gt.kind, deepcopy(gt.fields))
 Base.copy(l::Layout) = Layout(deepcopy(l.fields))
 Base.copy(p::Plot) = Plot([copy(t) for t in p.data], copy(p.layout))
 
-
-function to_svg(p::Plot)
+function to_svg(p::Plot, format="pdf")
     Blink.js(Plotlyjs.get_window(p), Blink.JSString("""
        var plt = document.getElementById('$(p.divid)');
-       Plotly.Snapshot.toSVG(plt, 'pdf')
+       Plotly.Snapshot.toSVG(plt, '$format')
        """))
 end
 
 # TODO: add width and height and figure out how to convert from measures to the
 #       pixels that will be expected in the SVG
-function savefig(p::Plot, fn::AbstractString; dpi::Real=300)
+function savefig(p::Plot, fn::AbstractString; dpi::Real=96)
     bas, ext = split(fn, ".")
     if !(ext in ["pdf", "png", "ps"])
         error("Only `pdf`, `png` and `ps` output supported")
@@ -42,7 +41,7 @@ function savefig(p::Plot, fn::AbstractString; dpi::Real=300)
     # write svg to tempfile
     temp = tempname()
     open(temp, "w") do f
-        write(f, to_svg(p))
+        write(f, to_svg(p, ext))
     end
 
     # hand off to cairosvg for conversion
@@ -52,6 +51,18 @@ function savefig(p::Plot, fn::AbstractString; dpi::Real=300)
     rm(temp)
 
     # return plot
+    p
+end
+
+# an alternative way to save plots -- no shelling out, but output less pretty
+function savefig2(p::Plot, fn::AbstractString)
+    # make sure plot window is active
+    show(p)
+    @eval begin
+        import ImageMagick, FileIO
+    end
+    img = ImageMagick.load_(base64decode(png_data(p)))
+    FileIO.save(fn, img)
     p
 end
 
@@ -84,34 +95,29 @@ function _img_data(p::Plot, format::ASCIIString)
         return to_svg(p)
     end
 
-    mime = "image/$format"
     # show the plot so we can access canvas
-    show(p)
+    # show(p)
     code = """
     var plt = document.getElementById('$(p.divid)');
-    var svg = Plotly.Snapshot.toSVG(plt);
-    var canvasContainer = window.document.createElement('div');
-    var canvas = window.document.createElement('canvas');
-    canvasContainer.appendChild(canvas);
-    var width = plt._fullLayout.width;
-    var height = plt._fullLayout.height;
-    var Image = window.Image;
-    var Blob = window.Blob;
-    var ctx = canvas.getContext('2d');
-    var img = new Image();
-    var DOMURL = window.URL || window.webkitURL;
-    var svgBlob = new Blob([svg], {type: 'image/svg+xml;charset=utf-8'});
-    var url = DOMURL.createObjectURL(svgBlob);
-    canvas.height = height;
-    canvas.width = width;
-
-    img.onload = function() {
-        ctx.drawImage(img, 0, 0);
-        DOMURL.revokeObjectURL(url);
-    }
-
-    img.src = url;
-    canvas.toDataURL('$mime');
+    var ev = Plotly.Snapshot.toImage(plt, {format: '$format'});
+    ev.once('success', function(imgData) {
+        var storage = document.createElement('p');
+        storage.id = 'theImageData';
+        storage.innerHTML = imgData;
+        storage.style.visibility = 'hidden';
+        storage.style.width = '0px';
+        storage.style.height = '0px';
+        document.head.appendChild(storage);
+        ev.clean();
+    });
     """
-    Blink.js(get_window(p), Blink.JSString(code))
+    _call_js(p, code)
+    sleep(0.2)  # give plotly time to fill the element with our data
+    new_code = """
+    var el = document.getElementById('theImageData');
+    var out = el.innerHTML;
+    el.remove()
+    out
+    """
+    Blink.js(get_window(p), Blink.JSString(new_code))
 end
