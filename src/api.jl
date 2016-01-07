d79c34f17a8d98c66ddb17d665f617da042f35a7
Spencer Lyon <spencerlyon2@gmail.com>
2016-01-07 00:54:47 -0700

    move js API methods to api.jl

--- src/api.jl
+++ src/api.jl
@@ -88,12 +88,135 @@ function _img_data(p::Plot, format::ASCIIString)
         error("Unsupported format $format, must be one of $_formats")
     end
 
-    if format == "svg"
-        return to_svg(p)
-    end
-
     @js p begin
         ev = Plotly.Snapshot.toImage(this, d("format"=>$format))
         @new Promise(resolve -> ev.once("success", resolve))
     end
+end
+
+
+# -------------- #
+# Javascript API #
+# -------------- #
+# TODO: update the fields on the Plot object also for functions that mutate the
+#       plot
+
+Blink.js(p::Plot, code::JSString; callback = true) =
+    Blink.js(get_window(p), :(Blink.evalwith(thediv, $(Blink.jsstring(code)))), callback = callback)
+
+restyle!(p::Plot, update = Dict(); kwargs...) =
+    @js_ p Plotly.restyle(this, $(merge(update, prep_kwargs(kwargs))))
+
+restyle!(p::Plot, traces::Integer...; kwargs...) =
+    @js_ p Plotly.restyle(this, $(prep_kwargs(kwargs)), $(collect(traces)))
+
+relayout!(p::Plot, update = Dict(); kwargs...) =
+    @js_ p Plotly.relayout(this, $(merge(update, prep_kwargs(kwargs))))
+
+addtraces!(p::Plot, traces::AbstractTrace...) =
+    @js_ p Plotly.addTraces(this, $traces)
+
+addtraces!(p::Plot, where::Union{Int,Vector{Int}}, traces::AbstractTrace...) =
+    @js_ p Plotly.addTraces(this, $traces, $where)
+
+deletetraces!(p::Plot, traces::Int...) =
+    @js_ p Plotly.deleteTraces(this, $(collect(traces)))
+
+movetraces!(p::Plot, to_end) =
+    @js_ p Plotly.moveTraces(this, $to_end)
+
+movetraces!(p::Plot, to_end...) = movetraces!(p, collect(to_end))
+
+movetraces!(p::Plot, src::Union{Int,Vector{Int}}, dest::Union{Int,Vector{Int}}) =
+    @js_ p Plotly.moveTraces(this, $src, $dest)
+
+redraw!(p::Plot) =
+    @js_ p Plotly.redraw(this)
+
+
+redraw!(p::Plot) =
+    @js_ p Plotly.redraw(this)
+
+# --------------------------------- #
+# unexported methods in plot_api.js #
+# --------------------------------- #
+
+tovec(v) = tovec([v])
+tovec(v::Vector) = eltype(v) <: Vector ? v : Vector[v]
+
+"""
+`extendtraces!(::Plot, ::Dict{Union{Symbol,AbstractString},Vector{Vector{Any}}}), indices, maxpoints)`
+
+Extend one or more traces with more data. A few notes about the structure of the
+update dict are important to remember:
+
+- The keys of the dict should be of type `Symbol` or `AbstractString` specifying
+  the trace attribute to be updated. These attributes must already exist in the
+  trace
+- The values of the dict _must be_ a `Vector` of `Vector` of data. The outer index
+  tells Plotly which trace to update, whereas the `Vector` at that index contains
+  the value to be appended to the trace attribute.
+
+These concepts are best understood by example:
+
+```julia
+# adds the values [1, 3] to the end of the first trace's y attribute and doesn't
+# remove any points
+extendtraces!(p, Dict(:y=>Vector[[1, 3]]), [0], -1)
+extendtraces!(p, Dict(:y=>Vector[[1, 3]]))  # equivalent to above
+```
+
+```julia
+# adds the values [1, 3] to the end of the third trace's marker.size attribute
+# and [5,5,6] to the end of the 5th traces marker.size -- leaving at most 10
+# points per marker.size attribute
+extendtraces!(p, Dict("marker.size"=>Vector[[1, 3], [5, 5, 6]]), [2, 4], 10)
+```
+
+"""
+function extendtraces!(p::Plot, update::Dict, indices::Vector{Int}=[0], maxpoints=-1;
+                       update_jl::Bool=false)
+    # update data in Julia object
+    if update_jl
+        for ix in indices
+            tr = p.data[ix+1]
+            for k in keys(update)
+                v = update[k][ix+1]
+                tr[k] = push!(tr[k], v...)
+            end
+        end
+    end
+
+    @js_ p Plotly.extendTraces(this, $update, $indices, $maxpoints)
+end
+
+"""
+The API for `prependtraces` is equivalent to that for `extendtraces` except that
+the data is added to the front of the traces attributes instead of the end. See
+Those docstrings for more information
+"""
+function prependtraces!(p::Plot, update::Dict, indices::Vector{Int}=[0], maxpoints=-1)
+    # update data in Julia object
+    if update_jl
+        for ix in indices
+            tr = p.data[ix+1]
+            for k in keys(update)
+                v = update[k][ix+1]
+                tr[k] = vcat(v, tr[k])
+            end
+        end
+    end
+    @js_ p Plotly.prependTraces(this, $update, $indices, $maxpoints)
+end
+
+
+for f in (:extendtraces!, :prependtraces!)
+    @eval $(f)(p::Plot, inds::Vector{Int}=[0], maxpoints=-1; update_jl=false, update...) =
+        ($f)(p, Dict(map(x->(x[1], tovec(x[2])), update)), inds, maxpoints; update_jl=update_jl)
+
+    @eval $(f)(p::Plot, inds::Int, maxpoints=-1; update_jl=false, update...) =
+        ($f)(p, [inds], maxpoints; update_jl=update_jl, update...)
+
+    @eval $(f)(p::Plot, update::Dict, inds::Int, maxpoints=-1; update_jl=false) =
+        ($f)(p, update, [inds], maxpoints; update_jl=update_jl)
 end
