8b4a9e208d48287c9505490a9f128e419da4705e
Spencer Lyon <spencerlyon2@gmail.com>
2016-03-15 09:02:26 -0400

    fix notebook setup to use require, not clobber it

--- src/displays/ijulia.jl
+++ src/displays/ijulia.jl
@@ -16,16 +16,17 @@ fork(jp::JupyterPlot) = JupyterPlot(fork(jp.plot))
 
 # if we're in IJulia call setupnotebook to load js and css
 if isdefined(Main, :IJulia) && Main.IJulia.inited
-    # the first script is some hack I needed to do in order for the notebook
-    # to not complain about Plotly being undefined
+    # borrowed from https://github.com/plotly/plotly.py/blob/2594076e29584ede2d09f2aa40a8a195b3f3fc66/plotly/offline/offline.py#L64-L71
     display("text/html", """
-        <script type="text/javascript">
-            require=requirejs=define=undefined;
+        <script type='text/javascript'>
+            define('plotly', function(require, exports, module) {
+                $(open(readall, _js_path, "r"))
+            });
+            require(['plotly'], function(Plotly) {
+                window.Plotly = Plotly;
+            });
         </script>
-        <script type="text/javascript">
-            $(open(readall, _js_path, "r"))
-        </script>
-     """)
+    """)
     display("text/html", "<p>Plotly javascript loaded.</p>")
     js_loaded(::JupyterDisplay) = true
 
