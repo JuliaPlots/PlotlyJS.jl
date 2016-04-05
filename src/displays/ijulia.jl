83e6c4fa663fcc3dfc954c92809752bc91cb9f5d
Spencer Lyon <spencerlyon2@gmail.com>
2016-04-05 10:56:44 -0400

    WIP: Hook up IJulia comm to do js interop

--- src/displays/ijulia.jl
+++ src/displays/ijulia.jl
@@ -5,11 +5,12 @@
 type JupyterDisplay <: AbstractPlotlyDisplay
     divid::Base.Random.UUID
     displayed::Bool
+    cond::Condition  # for getting data back from js
 end
 
 typealias JupyterPlot SyncPlot{JupyterDisplay}
 
-JupyterDisplay(p::Plot) = JupyterDisplay(p.divid, false)
+JupyterDisplay(p::Plot) = JupyterDisplay(p.divid, false, Condition())
 JupyterPlot(p::Plot) = JupyterPlot(p, JupyterDisplay(p))
 
 fork(jp::JupyterPlot) = JupyterPlot(fork(jp.plot))
@@ -32,24 +33,61 @@ function html_body(p::JupyterPlot)
     """
 end
 
-# if we're in IJulia call setupnotebook to load js and css
+# if we're in IJulia call setup the notebook js interop
 if isdefined(Main, :IJulia) && Main.IJulia.inited
     # borrowed from https://github.com/plotly/plotly.py/blob/2594076e29584ede2d09f2aa40a8a195b3f3fc66/plotly/offline/offline.py#L64-L71
+    # and https://github.com/JuliaLang/Interact.jl/blob/cc5f4cfd34687000bc6bc70f0513eaded1a7c950/src/IJulia/setup.jl#L15
     if !js_loaded(JupyterDisplay)
         display("text/html", """
-            <script type='text/javascript'>
-                define('plotly', function(require, exports, module) {
-                    $(open(readall, _js_path, "r"))
-                });
-                require(['plotly'], function(Plotly) {
-                    window.Plotly = Plotly;
-                });
-            </script>""")
+         <script charset="utf-8">
+            $(readall(joinpath(dirname(@__FILE__), "ijulia.js")))
+         </script>
+         <script charset="utf-8" type='text/javascript'>
+             define('plotly', function(require, exports, module) {
+                 $(open(readall, _js_path, "r"))
+             });
+             require(['plotly'], function(Plotly) {
+                 window.Plotly = Plotly;
+             });
+         </script>
+         <p>Plotly javascript loaded.</p>
+         """)
         _jupyter_js_loaded[1] = true
-        display("text/html", "<p>Plotly javascript loaded.</p>")
     end
 
-    @eval import IJulia
+    @eval begin
+        import IJulia
+        import IJulia.CommManager: Comm, send_comm
+    end
+
+    # set up the comms we will use to send js messages to be executed
+    const _ijulia_eval_comm = Comm(:plotlyjs_eval)
+    const _ijulia_return_comms = Dict{Base.Random.UUID,Comm}()
+
+    function get_comm(jd::JupyterDisplay)
+        if haskey(_ijulia_return_comms, jd.divid)
+            return _ijulia_return_comms[jd.divid]
+        else
+            comm = Comm(:plotlyjs_return)
+
+            function handle_comm_msg(msg)
+                if haskey(msg.content, "data")
+                    action = get(msg.content["data"], "action", "")
+                    if action == "plotlyjs_ret_val"
+                        val = msg.content["data"]["ret"]
+                        # now that we have the value, we can notify waiting
+                        # tasks that the return value is ready
+                        notify(jd.cond, val)
+                    end
+                end
+            end
+
+            comm.on_msg = handle_comm_msg
+
+            _ijulia_return_comms[jd.divid] = comm
+            return comm
+        end
+    end
 
     IJulia.display_dict(p::Plot) =
         Dict("text/plain" => sprint(writemime, "text/plain", p))
@@ -62,83 +100,82 @@ if isdefined(Main, :IJulia) && Main.IJulia.inited
             Dict("text/html" => html_body(p))
         end
     end
+
+end
+
+function _call_js_return(jd::JupyterDisplay, code)
+    # make sure plot has been displayed
+    display(jp)
+    send_comm(get_comm(jd), Dict("code" => code))  # will trigger `on_msg`
+    wait(jd.cond)  # wait for `notify` within `comm.on_msg` to be called
 end
 
 ## API Methods for JupyterDisplay
-function _img_data(p::JupyterPlot, format::ASCIIString)
-    # _formats = ["png", "jpeg", "webp", "svg"]
-    # if !(format in _formats)
-    #     error("Unsupported format $format, must be one of $_formats")
-    # end
-    #
-    # # make sure plot has been displayed
-    # display(p)
-    #
-    # # TODO: figure out how to resolve the promise
-    # display("text/html", """<script>
-    #     ev = Plotly.Snapshot.toImage(this, {'format': '$format'})
-    #     new Promise(resolve -> ev.once("success", resolve))
-    # </script>""")
-    error("Not implemented (yet). Use Electron frontend to save figures")
+_the_div_js(jd::JupyterDisplay) = "document.getElementById('$(jd.divid)')"
+_the_div_js(jp::JupyterPlot) = _the_div_js(jp.view)
+
+function _img_data(jp::JupyterPlot, format::ASCIIString)
+    _formats = ["png", "jpeg", "webp", "svg"]
+    if !(format in _formats)
+        error("Unsupported format $format, must be one of $_formats")
+    end
+
+    code =  """
+    ev = Plotly.Snapshot.toImage($(_the_div_js(jp)), {format: '$(format)'});
+    new Promise(function(resolve) {ev.once("success", resolve)});
+    """
+    _call_js_return(jp.view, code)
 end
 
 function svg_data(jp::JupyterPlot, format="png")
-    # display("text/html", """<script>
-    #     var thediv = document.getElementById('$(jp.view.divid)');
-    #     foobar = Plotly.Snapshot.toSVG(thediv, '$format')
-    # </script>""")
-    error("Not implemented (yet). Use Electron frontend to save figures")
+    code =  "Plotly.Snapshot.toSVG($(_the_div_js(jp)), '$(format)')"
+    _call_js_return(jp.view, code)
 end
 
-
-function call_plotlyjs(jd::JupyterDisplay, func::AbstractString, args...)
-    arg_str = length(args) > 0 ? string(",", join(map(json, args), ", ")) :
-                                 ""
-    script = "Plotly.$func(document.getElementById('$(jd.divid)') $arg_str)"
-    if jd.displayed  # only do this if the plot has been displayed
-        Base.HTML("<script>$script</script>")
-    else
-        Base.HTML("")
-    end
+function _call_plotlyjs(jd::JupyterDisplay, func::AbstractString, args...)
+    arg_str = length(args) > 0 ? string(",", join(map(json, args), ", ")) : ""
+    code = "Plotly.$func($(_the_div_js(jd)) $arg_str)"
+    jd.displayed && send_comm(_ijulia_eval_comm, Dict("code" => code))
+    nothing
 end
 
 # Methods from javascript API
 relayout!(jd::JupyterDisplay, update::Associative=Dict(); kwargs...) =
-    call_plotlyjs(jd, "relayout", merge(update, prep_kwargs(kwargs)))
+    _call_plotlyjs(jd, "relayout", merge(update, prep_kwargs(kwargs)))
 
 restyle!(jd::JupyterDisplay, ind::Int, update::Associative=Dict(); kwargs...) =
-    call_plotlyjs(jd, "restyle", merge(update, prep_kwargs(kwargs)), ind-1)
+    _call_plotlyjs(jd, "restyle", merge(update, prep_kwargs(kwargs)), ind-1)
 
 function restyle!(jd::JupyterDisplay, inds::AbstractVector{Int},
                   update::Associative=Dict();  kwargs...)
-    call_plotlyjs(jd, "restyle", merge(update, prep_kwargs(kwargs)), inds-1)
+    _call_plotlyjs(jd, "restyle", merge(update, prep_kwargs(kwargs)), inds-1)
 end
 
 restyle!(jd::JupyterDisplay, update::Associative=Dict(); kwargs...) =
-    call_plotlyjs(jd, "restyle", merge(update, prep_kwargs(kwargs)))
+    _call_plotlyjs(jd, "restyle", merge(update, prep_kwargs(kwargs)))
 
 addtraces!(jd::JupyterDisplay, traces::AbstractTrace...) =
-    call_plotlyjs(jd, "addTraces", traces)
+    _call_plotlyjs(jd, "addTraces", traces)
 
 addtraces!(jd::JupyterDisplay, where::Int, traces::AbstractTrace...) =
-    call_plotlyjs(jd, "addTraces", traces, where-1)
+    _call_plotlyjs(jd, "addTraces", traces, where-1)
 
 deletetraces!(jd::JupyterDisplay, traces::Int...) =
-    call_plotlyjs(jd, "deleteTraces", collect(traces)-1)
+    _call_plotlyjs(jd, "deleteTraces", collect(traces)-1)
 
 movetraces!(jd::JupyterDisplay, to_end::Int...) =
-    call_plotlyjs(jd, "moveTraces", collect(to_end)-1)
+    _call_plotlyjs(jd, "moveTraces", collect(to_end)-1)
 
 movetraces!(jd::JupyterDisplay, src::AbstractVector{Int}, dest::AbstractVector{Int}) =
-    call_plotlyjs(jd, "moveTraces", src-1, dest-1)
+    _call_plotlyjs(jd, "moveTraces", src-1, dest-1)
 
-redraw!(jd::JupyterDisplay) = call_plotlyjs(jd, "redraw")
+redraw!(jd::JupyterDisplay) = _call_plotlyjs(jd, "redraw")
 
 # unexported (by plotly.js) api methods
 extendtraces!(jd::JupyterDisplay, update::Associative=Dict(),
               indices::Vector{Int}=[1], maxpoints=-1;) =
-    call_plotlyjs(jd, "extendTraces", update, indices-1, maxpoints)
+    _call_plotlyjs(jd, "extendTraces", update, indices-1, maxpoints)
 
 prependtraces!(jd::JupyterDisplay, update::Associative=Dict(),
                indices::Vector{Int}=[1], maxpoints=-1;) =
-    call_plotlyjs(jd, "prependTraces", update, indices-1, maxpoints)
+    _call_plotlyjs(jd, "prependTraces", update, indices-1, maxpoints)
---
+++ src/displays/ijulia.js
@@ -0,0 +1,56 @@
+// This script was adapted from
+// https://github.com/JuliaLang/Interact.jl/blob/master/src/IJulia/ijulia.js
+(function (IPython, $) {
+    $.event.special.destroyed = {
+	remove: function(o) {
+	    if (o.handler) {
+		o.handler.apply(this, arguments)
+	    }
+	}
+    }
+
+    function resolve_promises(comm, val) {
+        val === undefined && (val = null);
+        if (val && val.constructor == Promise) {
+            val.then(val => resolve_promises(comm, val))
+        } else {
+            comm.send({action: "plotlyjs_ret_val", ret: val});
+        }
+    }
+
+    $(document).ready(function() {
+	function initComm(evt, data) {
+	    var comm_manager = data.kernel.comm_manager;
+        console.log("plotly comm init");
+	    comm_manager.register_target("plotlyjs_eval", function (comm) {
+            comm.on_msg(function (msg) {
+                // Call the code in the message
+                eval(msg.content.data.code);
+                delete msg.content.data.code;
+            });
+	    });
+
+        comm_manager.register_target("plotlyjs_return", function (comm) {
+            comm.on_msg(function (msg) {
+                // Call the code in the message
+                val = eval(msg.content.data.code);
+
+                // send the result back to julia
+                resolve_promises(comm, val);
+
+                // Clean up
+                delete val;
+                delete msg.content.data.code;
+            });
+	    });
+	}
+
+	try {
+	    // try to initialize right away. otherwise, wait on the status_started event.
+	    initComm(undefined, IPython.notebook);
+	} catch (e) {
+        console.log(e);
+	    $([IPython.events]).on('kernel_created.Kernel kernel_created.Session', initComm);
+	}
+    });
+})(IPython, jQuery);
