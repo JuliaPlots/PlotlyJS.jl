ce3f1fb840e7cabcf6eb1a25181e3b2d525e8ddb
Mike J Innes <mike.j.innes@gmail.com>
2016-10-07 14:59:41 +0100

    use juno default theme

--- src/displays/juno.jl
+++ src/displays/juno.jl
@@ -5,7 +5,10 @@ media(SyncPlot, Media.Plot)
 media(Plot, Media.Plot)
 
 function Media.render(::Juno.PlotPane, plot::SyncPlot)
+  style = plot.plot.style
+  style == Style() == DEFAULT_STYLE[1] && (plot.plot.style = gadfly_dark_style())
   display_blink(plot)
+  plot.plot.style = style
 end
 
 @render Juno.PlotPane plot::Plot SyncPlot(plot)
