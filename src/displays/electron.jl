6f793cf19b62237e5628392db3f82e8ca4694629
Spencer Lyon <spencerlyon2@gmail.com>
2016-12-02 13:50:02 -0500

    ENH: rework much of the electron display:
    - Main benefit is now savefig should be very reliable
    - I changed the behavior so that immediately after displaying a plot, I
    ask plotly.js to do async operations to generate the svg for the figure.
    I then store this in a global variable (yuck!) and just access it when I
    want to savefig
    - The downside here is that any restyle! or relayout! operations won't
    be included in the svg data. I think I can probably overcome that, but
    It isn't totally obvious to me how yet...

--- src/displays/electron.jl
+++ src/displays/electron.jl
@@ -5,101 +5,183 @@
 type ElectronDisplay <: AbstractPlotlyDisplay
     divid::Base.Random.UUID
     w::Nullable{Any}
-    js_loaded::Bool
 end
 
 typealias ElectronPlot SyncPlot{ElectronDisplay}
 
-ElectronDisplay(divid::Base.Random.UUID) = ElectronDisplay(divid, Nullable(), false)
+ElectronDisplay(divid::Base.Random.UUID) = ElectronDisplay(divid, Nullable())
 ElectronDisplay(p::Plot) = ElectronDisplay(p.divid)
 ElectronPlot(p::Plot) = ElectronPlot(p, ElectronDisplay(p.divid))
 
 fork(jp::ElectronPlot) = ElectronPlot(fork(jp.plot), ElectronDisplay())
 
+# define some core methods on the display
+
+"""
+Return true if the display has been initialized and is still open, else false
+"""
 isactive(ed::ElectronDisplay) = isnull(ed.w) ? false : Blink.active(get(ed.w))
 
+"""
+If the display is active, close the window
+"""
 Base.close(ed::ElectronDisplay) = isactive(ed) && close(get(ed.w))
 
-function get_window(p::ElectronPlot; kwargs...)
-    w, h = size(p.plot)
-    get_window(p.view; width=w, height=h, kwargs...)
+
+"Variable we can query to see if the plot has finished rendering"
+function done_var(ed::ElectronDisplay)
+    Symbol("done_$(split(string(ed.divid), "-")[1])")
 end
 
+"variable to store the svg data in"
+function svg_var(ed::ElectronDisplay)
+    Symbol("svg_$(split(string(ed.divid), "-")[1])")
+end
+
+"""
+Return true if the display is active and the plot has finished rendering, else
+false
+"""
+function Base.isready(ed::ElectronDisplay)
+    !isactive(ed) && return false
+    Blink.js(ed, done_var(ed))
+end
+
+# helper method to obtain a Blink window, given some options
+get_window(opts::Dict) = Juno.isactive() ? Juno.Atom.blinkplot() : Window(opts)
+
+"""
+Initialize a Blink window for displaying the a plot. This will return an
+existing window if it has been initialized and is still active, otherwise it
+creates one.
+
+Part of the creation process here is loading the plotly javascript.
+"""
 function get_window(ed::ElectronDisplay; kwargs...)
     if !isnull(ed.w) && active(get(ed.w))
         w = get(ed.w)
     else
         w = get_window(Dict(kwargs))
         ed.w = w
-        ed.js_loaded = false  # can't have js if we made a new window
+        # load the js here
+        Blink.load!(w, _js_path)
+        Blink.loadjs!(w, _mathjax_cdn_path)
+        Blink.js(w, :($(done_var(ed)) = false))
+        Blink.js(w, :($(svg_var(ed)) = undefined))
+
+        # make sure plotly has been loaded
+        done = false
+        while !done
+            try
+                @js w Plotly
+                done = true
+            catch e
+                continue
+            end
+        end
     end
     w
 end
 
-get_window(opts::Dict) = Juno.isactive() ? Juno.Atom.blinkplot() : Window(opts)
+# the corresponding methods for ElectronPlot can (for the most part)
+# just forward on to the view
+Base.isready(p::ElectronPlot) = isready(p.view)
 
-js_loaded(ed::ElectronDisplay) = ed.js_loaded
+done_var(p::ElectronPlot) = done_var(p.view)
+svg_var(p::ElectronPlot) = svg_var(p.view)
 
-function loadjs(ed::ElectronDisplay)
-    if !ed.js_loaded
-        Blink.loadjs!(get_window(ed), _mathjax_cdn_path)
-        Blink.load!(get_window(ed), _js_path)
-        ed.js_loaded = true
-    end
+"""
+Close the plot window (if active) and reset the fields and reset the `w` field
+on the display to be `Nullable()`.
+"""
+function Base.close(p::ElectronPlot)
+    close(p.view)
+    p.view.w = Nullable()
+    nothing
+end
+
+"""
+Use the plot width and height to open a window of the correct size
+"""
+function get_window(p::ElectronPlot; kwargs...)
+    w, h = size(p.plot)
+    get_window(p.view; width=w, height=h, kwargs...)
 end
 
 Base.display(p::ElectronPlot; show = true, resize::Bool=_autoresize[1]) =
   display_blink(p; show = show, resize = resize)
 
-function display_blink(p::ElectronPlot; show=true, resize::Bool=_autoresize[1])
+function display_blink(p::ElectronPlot; show=true, resize::Bool=_autoresize[1],)
     w = get_window(p, show=show)
-    loadjs(p.view)
-    lowered = JSON.lower(p.plot)
+
+    done = done_var(p)
+    svg = svg_var(p)
+
     if !resize
-        @js w begin
-            trydiv = document.getElementById($(string(p.plot.divid)))
-            if trydiv == nothing
-                @var gd = document.createElement("div")
-                gd.id = $(string(p.plot.divid))
-                document.body.appendChild(gd)
-            else
-                @var gd = trydiv
-            end
-            @var _ = Plotly.newPlot(gd, $(json(lowered[:data])),
-            $(json(lowered[:layout])),
-            d("showLink"=> false))
-            _.then(()->Promise.resolve())
-        end
+        code = """
+        <script>
+        (function(){
+        var gd = Plotly.d3.select("body")
+            .append("div")
+            .attr("id", "$(p.plot.divid)")
+            .node();
+        var data = $(json(p.plot.data));
+        var layout = $(json(p.plot.layout));
+        Plotly.newPlot(gd, data, layout).then(function(gd) {
+                 $(done) = true;
+                 return Plotly.toImage(gd, {"format": "svg"});
+            }).then(function(data) {
+                var svg_data = data.replace(/^data:image\\/svg\\+xml,/, "");
+                $(svg) = decodeURIComponent(svg_data);
+             });
+         })();
+        </script>
+        """
+        @js(w, Blink.fill("body", $code))
     else
-        # remove the width and height fields on the layout or
-        # the autoresize won't work
-        pop!(p.plot.layout.fields, :width, nothing)
-        pop!(p.plot.layout.fields, :height, nothing)
+        #= this is the same as above, with a few differences:
+
+        - when we create the div, set some style attributes so the div itself
+        grows with the display window
+        - After obtaining the image data, we delete the width and height
+        properties on the layout so that if the user happened to have set them
+        on the Julia side, they will be removed -- allowing the plot to always
+        fill the div
+        =#
         magic = """
         <script>
         (function() {
-            var d3 = Plotly.d3
             var WIDTH_IN_PERCENT_OF_PARENT = 100
             var HEIGHT_IN_PERCENT_OF_PARENT = 100;
-            var gd3 = d3.select('body')
-            .append('div').attr("id", "$(p.plot.divid)")
-            .style({
-                width: WIDTH_IN_PERCENT_OF_PARENT + '%',
-                'margin-left': (100 - WIDTH_IN_PERCENT_OF_PARENT) / 2 + '%',
-                height: HEIGHT_IN_PERCENT_OF_PARENT + 'vh',
-                'margin-top': (100 - HEIGHT_IN_PERCENT_OF_PARENT) / 2 + 'vh'
-            });
-            var gd = gd3.node();
+            var gd = Plotly.d3.select('body')
+                .append('div').attr("id", "$(p.plot.divid)")
+                .style({
+                    width: WIDTH_IN_PERCENT_OF_PARENT + '%',
+                    'margin-left': (100 - WIDTH_IN_PERCENT_OF_PARENT) / 2 + '%',
+                    height: HEIGHT_IN_PERCENT_OF_PARENT + 'vh',
+                    'margin-top': (100 - HEIGHT_IN_PERCENT_OF_PARENT) / 2 + 'vh'
+                })
+                .node();
             var data = $(json(p.plot.data));
-            var layouts = $(json(p.plot.layout));
-            Plotly.newPlot(gd, data, layouts);
+            var layout = $(json(p.plot.layout));
+            Plotly.newPlot(gd, data, layout).then(function(gd) {
+                     $(done) = true;
+                     var img_data = Plotly.toImage(gd, {"format": "svg"});
+                     delete gd.layout.width
+                     delete gd.layout.height
+                     return img_data
+                }).then(function(img_data) {
+                    var svg_data = img_data.replace(/^data:image\\/svg\\+xml,/, "");
+                    $(svg) = decodeURIComponent(svg_data);
+                 });
             window.onresize = function() {
-            Plotly.Plots.resize(gd);
-            };
-        })();
+                Plotly.Plots.resize(gd);
+                };
+            }
+        )();
         </script>
         """
-        Blink.body!(w, magic)
+        @js(w, Blink.fill("body", $magic))
     end
     p.plot
 end
@@ -122,18 +204,21 @@ function _img_data(p::ElectronPlot, format::String; show::Bool=false)
     out
 end
 
-function svg_data(p::ElectronPlot, format="png")
+function svg_data(p::ElectronPlot, format="png", robust=false)
     opened_here = !isactive(p.view)
-    opened_here && display(p; show=false, resize=false)
+    opened_here && (display(p; show=false, resize=false); sleep(0.1))
 
-    out = @js p.view Plotly.Snapshot.toSVG(this, $format)
+    out = nothing
+    while out === nothing
+        out = @js p $(svg_var(p))
+        # wait for plot to render
+        sleep(0.2)
+    end
 
     opened_here && close(p)
 
     return out
 end
-
-Base.close(p::ElectronPlot) = close(p.view)
 
 function Blink.js(p::ElectronDisplay, code::JSString; callback=true)
     if !isactive(p)
