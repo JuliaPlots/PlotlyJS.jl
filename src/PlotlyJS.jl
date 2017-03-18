3f27901e195cae6d73dcd9d500cca8f3f61f17cb
Spencer Lyon <spencerlyon2@gmail.com>
2017-03-17 20:14:06 -0400

    BUG: typo in docs function

--- src/PlotlyJS.jl
+++ src/PlotlyJS.jl
@@ -73,7 +73,7 @@ end
 
 function docs()
     schema_path = joinpath(dirname(dirname(@__FILE__)), "deps", "schema.html")
-    if !isfile(schem_path)
+    if !isfile(schema_path)
         msg = "schema docs not built. Run `Pkg.build(\"PlotlyJS\")` to generate"
         error(msg)
     end
