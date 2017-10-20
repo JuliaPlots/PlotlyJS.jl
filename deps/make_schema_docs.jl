7ba5516208a868f83a18768380b7c8d319101adf
Spencer Lyon <spencerlyon2@gmail.com>
2017-10-20 17:38:00 -0400

    BUG: fix invalid escape sequence bug on julia 0.7-

--- deps/make_schema_docs.jl
+++ deps/make_schema_docs.jl
@@ -97,7 +97,7 @@ function doc_html!(buf::IO, parent::Symbol, name::Symbol, sa::SchemaAttribute)
     print(buf,"<div class=\"panel")
     !isnull(sa.children) && print(buf, " panel-info")
     print(buf, "\">")
-    print(buf, """\
+    print(buf, """
      <div class="panel-heading">
        <h5 class="panel-title">
          <a data-toggle="collapse" data-parent="""
@@ -150,7 +150,7 @@ end
 
 function doc_html!(buf::IO, data_parent::Symbol, name::Symbol, ts::TraceSchema)
     id = "$(data_parent)_$(name)"
-    print(buf, """\
+    print(buf, """
     <div class="panel panel-default">
      <div class="panel-heading">
       <h4 class="panel-title">
