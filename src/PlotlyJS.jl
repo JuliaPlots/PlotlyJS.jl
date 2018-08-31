780f664aeba27a22d0fe7a1bfc12c7cfc7679991
cstjean <cedric.stjean@gmail.com>
2018-08-31 12:23:36 -0400

    Import html_body

--- src/PlotlyJS.jl
+++ src/PlotlyJS.jl
@@ -11,7 +11,7 @@ import PlotlyBase:
     restyle!, relayout!, update!, addtraces!, deletetraces!, movetraces!,
     redraw!, extendtraces!, prependtraces!, purge!, to_image, download_image,
     restyle, relayout, update, addtraces, deletetraces, movetraces, redraw,
-    extendtraces, prependtraces, prep_kwargs, sizes, savefig, _tovec
+    extendtraces, prependtraces, prep_kwargs, sizes, savefig, _tovec, html_body
 
 using WebIO
 using JSExpr
