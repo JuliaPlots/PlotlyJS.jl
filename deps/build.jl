5770d98ce211aff2d61b1ed66e7efc3649d7f9b9
Jonathan Stickel <jjstickel@gmail.com>
2019-02-01 20:47:38 -0700

    Check for data files before downloading them during build.  (#237)
    * If data files fail to download, check if the exist before providing an error.
    Occassionally users
    cannot use curl for downloading (e.g., firewall), and the workaround is to
    download the files via web browser and place them in the proper location.
    
    * Add a check to __init__() for warnings in build.log
    
    Because @warn (and @info) do not write out to the REPL during the build phase,
    this commit adds a check during __init__() for warnings in the build.log and
    prompts the user to check it.

--- deps/build.jl
+++ deps/build.jl
@@ -4,8 +4,23 @@ const _pkg_assets = joinpath(_pkg_root,"assets")
 
 !isdir(_pkg_assets) && mkdir(_pkg_assets)
 
-download("https://api.plot.ly/v2/plot-schema?sha1", joinpath(_pkg_deps,"plotschema.json"))
-download("https://cdn.plot.ly/plotly-latest.min.js", joinpath(_pkg_assets,"plotly-latest.min.js"))
+function datadown(thepath, thefile, theurl)
+    filepath = joinpath(thepath, thefile)
+    try
+        download(theurl, filepath)
+    catch 
+        if isfile(filepath)
+            @warn("Failed to update $thefile, but you already have it. Things might continue to work, but if you would like to make sure you have the latest version of $thefile, use may use your web-browser to download it from $theurl and place it in $_pkg_deps.")
+        else
+            error("Failed to download $thefile from $theurl. You may use your web-browser to download it from $theurl and place it in $_pkg_deps.")
+        end
+    end
+end
+
+datadown(_pkg_deps, "plotschema.json",
+         "https://api.plot.ly/v2/plot-schema?sha1")
+datadown(_pkg_assets, "plotly-latest.min.js",
+         "https://cdn.plot.ly/plotly-latest.min.js")
 
 include("make_schema_docs.jl")
 include("find_attr_groups.jl")
