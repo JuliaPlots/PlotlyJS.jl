9aa62a9c7e3265b7a2594f122c893a41e48d27bf
Jonas Krimmer <30567171+jonas-kr@users.noreply.github.com>
2021-03-18 13:57:00 +0100

    Correctly generate artifact

--- deps/generate_artifacts.jl
+++ deps/generate_artifacts.jl
@@ -1,10 +1,9 @@
 # This script gets run once, on a developer's machine, to generate artifacts
 using Pkg.Artifacts
-using SHA
 
 artifacts_toml = joinpath(dirname(@__DIR__), "Artifacts.toml")
 
-rm(artifacts_toml,force=true)
+rm(artifacts_toml)
 
 plotschema_url = "https://api.plot.ly/v2/plot-schema?sha1"
 plotly_url = "https://cdn.plot.ly/plotly-latest.min.js"
@@ -14,10 +13,8 @@ plotlyartifacts_hash = create_artifact() do artifact_dir
     download(plotly_url, joinpath(artifact_dir, "plotly-latest.min.js"))
 end
 
-artifact_dir = artifact_path(plotlyartifacts_hash)
-bind_artifact!(artifacts_toml, "plotly-artifacts", plotlyartifacts_hash; download_info = [
-    (plotschema_url, bytes2hex(sha256(read(joinpath(artifact_dir, "plotschema.json"))))),
-    (plotly_url, bytes2hex(sha256(read(joinpath(artifact_dir, "plotly-latest.min.js"))))),
-])
+tarball_hash = archive_artifact(plotlyartifacts_hash, "plotly-artifacts.tar.gz")
 
-archive_artifact(plotlyartifacts_hash, "plotly-artifacts.tar.gz")
+bind_artifact!(artifacts_toml, "plotly-artifacts", plotlyartifacts_hash; download_info = [
+    ("https://github.com/jonas-kr/PlotlyJS.jl/releases/download/plotly-artifacts/plotly-artifacts.tar.gz", tarball_hash)
+])
