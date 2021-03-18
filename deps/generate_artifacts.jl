488e753f245cd49d349fcf349ceec82913cc2120
Jonas Krimmer <30567171+jonas-kr@users.noreply.github.com>
2021-03-18 12:42:39 +0100

    Generate single artifact

--- deps/generate_artifacts.jl
+++ deps/generate_artifacts.jl
@@ -1,22 +1,23 @@
 # This script gets run once, on a developer's machine, to generate artifacts
 using Pkg.Artifacts
+using SHA
 
 artifacts_toml = joinpath(dirname(@__DIR__), "Artifacts.toml")
 
-rm(artifacts_toml)
+rm(artifacts_toml,force=true)
 
 plotschema_url = "https://api.plot.ly/v2/plot-schema?sha1"
-plotschema_hash = create_artifact() do artifact_dir
-    download(plotschema_url, joinpath(artifact_dir, "plotschema.json"))
-end
-bind_artifact!(artifacts_toml, "plotschema", plotschema_hash; download_info = [
-    (plotschema_url, plotschema_hash)
-])
-
 plotly_url = "https://cdn.plot.ly/plotly-latest.min.js"
-plotly_hash = create_artifact() do artifact_dir
+
+plotlyartifacts_hash = create_artifact() do artifact_dir
+    download(plotschema_url, joinpath(artifact_dir, "plotschema.json"))
     download(plotly_url, joinpath(artifact_dir, "plotly-latest.min.js"))
 end
-bind_artifact!(artifacts_toml, "plotly", plotly_hash; download_info = [
-    (plotly_url, plotly_hash)
+
+artifact_dir = artifact_path(plotlyartifacts_hash)
+bind_artifact!(artifacts_toml, "plotly-artifacts", plotlyartifacts_hash; download_info = [
+    (plotschema_url, bytes2hex(sha256(read(joinpath(artifact_dir, "plotschema.json"))))),
+    (plotly_url, bytes2hex(sha256(read(joinpath(artifact_dir, "plotly-latest.min.js"))))),
 ])
+
+archive_artifact(plotlyartifacts_hash, dirname(@__DIR__))
