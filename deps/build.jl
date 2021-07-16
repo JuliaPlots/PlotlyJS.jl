e47fe852d8c830eec74d6bc2e86300d6660d129c
Spencer Lyon <spencerlyon2@gmail.com>
2021-07-16 09:32:50 -0600

    update to plotly 2.x!

--- deps/build.jl
+++ deps/build.jl
@@ -1,9 +1,7 @@
 const _pkg_root = dirname(dirname(@__FILE__))
-const _pkg_deps = joinpath(_pkg_root,"deps")
-const _pkg_assets = joinpath(_pkg_root,"assets")
+const _pkg_deps = joinpath(_pkg_root, "deps")
+const _pkg_assets = joinpath(_pkg_root, "assets")
 
 !isdir(_pkg_assets) && mkdir(_pkg_assets)
 
 include("make_schema_docs.jl")
-include("find_attr_groups.jl")
-AttrGroups.main()
