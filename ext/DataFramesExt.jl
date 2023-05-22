2727c6688674d0742131b4f35f34deb9563f2568
Steve Kelly <kd2cca@gmail.com>
2023-05-22 09:39:51 -0400

    Update ext/DataFramesExt.jl
    Co-authored-by: Thomas Christensen <tchr@mit.edu>

--- ext/DataFramesExt.jl
+++ ext/DataFramesExt.jl
@@ -5,6 +5,6 @@ isdefined(Base, :get_extension) ? (using DataFrames) : (using ..DataFrames)
 isdefined(Base, :get_extension) ? (using CSV) : (using ..CSV)
 
 
-dataset(::Type{DataFrames.DataFrame}, name::String) = DataFrames.DataFrame(PlotlyJS.dataset(CSV.File, name))
+PlotlyJS.dataset(::Type{DataFrames.DataFrame}, name::String) = DataFrames.DataFrame(PlotlyJS.dataset(CSV.File, name))
 
 end
