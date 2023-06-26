7e04f1ff0aee8935456c510cc4d3da250a1abe1b
Simon Christ <simonchrist@gmx.de>
2023-06-26 17:10:39 +0200

    fix loading issues

--- ext/IJuliaExt.jl
+++ ext/IJuliaExt.jl
@@ -1,11 +1,12 @@
 module IJuliaExt
 
+using PlotlyJS
 isdefined(Base, :get_extension) ? (using IJulia) : (using ..IJulia)
 isdefined(Base, :get_extension) ? (using JSON) : (using ..JSON)
 isdefined(Base, :get_extension) ? (using PlotlyBase) : (using ..PlotlyBase)
 
 
-function IJulia.display_dict(p::PlotlyBase.SyncPlot)
+function IJulia.display_dict(p::PlotlyJS.SyncPlot)
     Dict(
         "application/vnd.plotly.v1+json" => JSON.lower(p),
         "text/plain" => sprint(show, "text/plain", p),
--- ext/JSON3Ext.jl
+++ ext/JSON3Ext.jl
@@ -1,10 +1,9 @@
 module JSON3Ext
 
-isdefined(Base, :get_extension) ? (using PlotlyBase) : (using ..PlotlyBase)
-isdefined(Base, :get_extension) ? (using JSON) : (using ..JSON)
+using PlotlyJS
 isdefined(Base, :get_extension) ? (using JSON3) : (using ..JSON3)
 
-JSON3.write(io::IO, p::PlotlyBase.SyncPlot) = JSON.print(io, p.plot)
-JSON3.write(p::PlotlyBase.SyncPlot) = JSON.json(p.plot)
+JSON3.write(io::IO, p::PlotlyJS.SyncPlot) = JSON3.write(io, p.plot)
+JSON3.write(p::PlotlyJS.SyncPlot) = JSON3.write(p.plot)
 
 end
