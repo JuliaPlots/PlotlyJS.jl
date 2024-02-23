1203477a9ad00835f89e38a0ac20a8ebbe38cd27
Simon Christ <christ@cell.uni-hannover.de>
2024-02-23 13:27:43 +0100

    dont spawn task

--- src/PlotlyJS.jl
+++ src/PlotlyJS.jl
@@ -99,7 +99,7 @@ function __init__()
         @warn("Warnings were generated during the last build of PlotlyJS:  please check the build log at $_build_log")
     end
 
-    kaleido_task = Base.Threads.@spawn PlotlyKaleido.start()
+    kaleido_task = PlotlyKaleido.start()
 
     if !isfile(_js_path)
         @info("plotly.js javascript libary not found -- downloading now")
